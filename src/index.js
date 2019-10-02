b0e3a5f4fb4a851f1849fa269168e10c95a8de6f
Svetlana Plinda <s.plinda@godeltech.com>
2019-10-02 16:13:32 +0300

    add searchForm

---
+++ src/index.js
@@ -0,0 +1,20 @@
+let $root = document.querySelector("#root");
+
+$root.innerHTML = `
+    <form id="seachForm">
+        <input id="searchField" name="searchField"/>
+        <button>search</button>
+    </form>
+    <button></button>
+    <div id="field"></div>
+`;
+
+let $seachForm = document.querySelector("#seachForm");
+let $searchField = $seachForm.querySelector("#searchField");
+
+$seachForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let username = $searchField.value;
+    su(username)
+        .then(renderUsers)
+})
