08c16ec07416c7110126cb77c1227b44929d8759
Eugene <skullet-on@mail.ru>
2019-10-04 12:42:31 +0300

    Created header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,47 +1,16 @@
-import { searchUsers } from "./fetch";
-import { sortUsers } from "./sortUsers"
+import React from 'react';
+import './header.css';
 
-let $root = document.querySelector("#root");
-
-$root.innerHTML = `
-    <form id="seachForm">
-        <input id="searchField" name="searchField"/>
-        <button>search</button>
-    </form>
-    <button id="sortUsers">sort</button>
-    <div id="usersList"></div>
-`;
-
-let gitUsers = [];
-let $seachForm = document.querySelector("#seachForm");
-let $searchField = $seachForm.querySelector("#searchField");
-let $usersList = document.querySelector("#usersList");
-let $sortUsersBtn = document.querySelector("#sortUsers");
-let userSortDirection;
-
-$seachForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-  let userName = $searchField.value;
-
-  searchUsers(userName)
-    .then(items => {
-      gitUsers = items;
-      renderUsers(gitUsers);
-    });
-})
-
-$sortUsersBtn.addEventListener("click", sorting);
-
-function renderUsers(items) {
-  $usersList.innerHTML = items.map(el => `
-    <div class="User">
-        <img src="${el.avatar_url}"  alt="UserPic">
-        <span> ${el.login}</span>
+class Header extends React.Component {
+  render (){
+    return <div className="header">
+      <form className="searchUsersForm" id="searchUsersForm">
+        <input className="searchUsersField" name="searchUsersField" />
+        <input className="searchUsersSubmit" type="submit" value="Search users" />
+      </form>
+      <input className="sortUsersBtn" type="button" value="Sort users"/>
     </div>
-    `).join('');
+  }
 }
 
-function sorting() {
-  userSortDirection = (userSortDirection === 'desc') ? 'asc' : 'desc';
-  renderUsers(sortUsers(gitUsers, userSortDirection))
-}
+export default Header;
