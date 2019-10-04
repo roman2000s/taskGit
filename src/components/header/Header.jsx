deb0ea9ddff560d4da6585b48423744affce7d4a
Eugene <skullet-on@mail.ru>
2019-10-04 15:50:26 +0300

    added Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './header.css';
 
-class Header extends React.Component {
-  render (){
+class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { userlogin: '' }
+  }
+
+  handleGetUserLogin(e) {
+    e.preventDefault();
+    this.state.userlogin ? this.props.onSearchUsers(this.state.userlogin) : alert("Login should not be empty");
+  }
+
+  handleChangeSUF(e) {
+    this.setState({
+      userlogin: e.target.value
+    })
+  }
+
+  render() {
     return <div className="header">
-      <form className="searchUsersForm" id="searchUsersForm">
-        <input className="searchUsersField" name="searchUsersField" />
-        <input className="searchUsersSubmit" type="submit" value="Search users" />
+      <form className="searchUsersForm" id="searchUsersForm" onSubmit={(e) => this.handleGetUserLogin(e)}>
+        <span>Login: </span>
+        <input 
+          className="searchUsersField" 
+          name="searchUsersField"
+          value={this.state.userlogin}
+          onChange={e => this.handleChangeSUF(e)} />
+        <input 
+          className="searchUsersSubmit" 
+          type="submit" 
+          value="Search users" />
       </form>
       <input className="sortUsersBtn" type="button" value="Sort users"/>
     </div>
