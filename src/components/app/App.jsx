407ef789aacbe81c5d8318cd17836796793166cd
RomanShestakov <r.shestakov@godeltech.com>
2019-10-04 12:07:06 +0300

    make app classComponent

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div></div>
+    )
+  }
 }
 
 export default App;
