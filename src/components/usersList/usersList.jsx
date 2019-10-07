7ea195161f41264a6d4dcf323aad7280bda294b3
Eugene <skullet-on@mail.ru>
2019-10-07 11:58:57 +0300

    added render users

---
+++ src/components/usersList/usersList.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import "./usersList.css";
+
+class UserList extends Component {
+  renderUsersList(users) {
+    console.log(users)
+    return <div className="userList">
+      {
+        users.map(user => {
+          return <div className="userInfo">
+            <div className="userLogin">{user.login}</div>
+            <img src={user.avatar_url} alt={user.login} />
+          </div>
+        })
+      }
+    </div>
+  }
+
+  renderEmptyList() {
+    return <div className="usersList">
+        Users not found
+      </div>
+  }
+
+  render() {
+    return this.props.usersList ? this.renderUsersList(this.props.usersList) : this.renderEmptyList()
+  }
+}
+
+export default UserList;
