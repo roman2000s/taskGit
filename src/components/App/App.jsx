2fa2a1788d96eb00c63a69156352fb0c4a0b79e7
Eugene <skullet-on@mail.ru>
2019-10-10 16:35:23 +0300

    Added eslint to project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,39 @@
-import React, { useState } from 'react'
-import { Switch, Route, Redirect, useHistory } from 'react-router-dom'
-import Header from '../Header/Header'
-import UsersListPage from '../UsersListPage/UsersListPage'
-import UserPage from '../UserPage/UserPage'
+import React, { useState } from 'react';
+import {
+  Switch, Route, Redirect, useHistory,
+} from 'react-router-dom';
+import Header from '../Header/Header';
+import UsersListPage from '../UsersListPage/UsersListPage';
+import UserPage from '../UserPage/UserPage';
 
 export default function App() {
-  let [sort, setSort] = useState('desc')
-  let [search, setSearch] = useState('')
-  let history = useHistory()
+  const [sort, setSort] = useState('desc');
+  const [search, setSearch] = useState('');
+  const history = useHistory();
 
-  return <>
-    <Header
-      onSearch={(query) => {
-        history.push('/users')
-        setSearch(query)
-      }}
-      onSortChange={setSort}
-      sort={sort}
-    />
+  return (
+    <>
+      <Header
+        onSearch={(query) => {
+          history.push('/users');
+          setSearch(query);
+        }}
+        onSortChange={setSort}
+        sort={sort}
+      />
 
-    <Switch>
-      <Route
-        exact
-        path="/users"
-        render={() => <UsersListPage search={search} sort={sort} />}
-      />
-      <Route
-        path="/users/:username"
-        render={({ match }) => <UserPage username={match.params.username} />}
-      />
-      <Route render={() => <Redirect to="/users" />} />
-    </Switch>
-  </>
+      <Switch>
+        <Route
+          exact
+          path="/users"
+          render={() => <UsersListPage search={search} sort={sort} />}
+        />
+        <Route
+          path="/users/:username"
+          render={({ match }) => <UserPage username={match.params.username} />}
+        />
+        <Route render={() => <Redirect to="/users" />} />
+      </Switch>
+    </>
+  );
 }
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,24 +1,65 @@
-import React, { useState, useEffect } from 'react'
-import { searchUser } from '../../helpers/searchUsers'
-import styles from './UserPage.module.css'
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { searchUser } from '../../helpers/searchUsers';
+import styles from './UserPage.module.css';
 
 export default function UserPage({ username }) {
-  let [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     searchUser(username)
-      .then(setUser)
-  }, [username])
+      .then(setUser);
+  }, [username]);
 
-  if (!user) return <div className={styles.user}>No user</div>
+  if (!user) return <div className={styles.user}>No user</div>;
 
-  return <div className={styles.user}>
-    <img src={user.avatar_url} alt={user.login} className={styles.avatar} />
-    {user.login ? <div>Login: {user.login}</div> : false}
-    {user.name ? <div>Name: {user.name}</div> : false}
-    {user.email ? <div>Email: {user.email}</div> : false}
-    {user.company ? <div>Company: {user.company}</div> : false}
-    {user.blog ? <div>Blog: {user.blog}</div> : false}
-    {user.created_at ? <div>Created: {user.created_at}</div> : false}
-  </div>
+  return (
+    <div className={styles.user}>
+      <img src={user.avatar_url} alt={user.login} className={styles.avatar} />
+      {user.login ? (
+        <div>
+          <span>Login:</span>
+          {user.login}
+        </div>
+      ) : false}
+      {user.name ? (
+        <div>
+          <span>Name:</span>
+          {user.name}
+        </div>
+      ) : false}
+      {user.email ? (
+        <div>
+          <span>Email:</span>
+          {user.email}
+        </div>
+      ) : false}
+      {user.company ? (
+        <div>
+          <span>Company:</span>
+          {user.company}
+        </div>
+      ) : false}
+      {user.blog ? (
+        <div>
+          <span>Blog:</span>
+          {user.blog}
+        </div>
+      ) : false}
+      {user.created_at ? (
+        <div>
+          <span>Created:</span>
+          {user.created_at}
+        </div>
+      ) : false}
+    </div>
+  );
 }
+
+UserPage.propTypes = {
+  username: PropTypes.string,
+};
+
+UserPage.defaultProps = {
+  username: '',
+};
