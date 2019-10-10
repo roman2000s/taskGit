512bca3362671da40cf570db29d6e2204c99a773
Eugene <skullet-on@mail.ru>
2019-10-10 12:58:02 +0300

    rewrite application on react hooks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app/App";
+import App from "./components/App/App";
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(<Router>
