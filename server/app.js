6f537ea78c955e2c75833ce3c15efc5a46ffe2fd
Eugene <skullet-on@mail.ru>
2019-10-14 16:08:16 +0300

    added /users and /users/:userlogin routes

--- server/app.js
+++ server/app.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
+
 const PORT = 3001;
+const USERS = require('./mockedData');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+  let users = []
+
+  USERS.items.map( (user) => {
+    user.login.includes(req.query.q) ? users.push(user) : false
+  })
+  res.json(users);
+});
+
+app.get('/users/:username', (req, res) => {
+  let response = USERS.items.find(user => {
+    if (user.login === req.params.username)
+      return res.json(user)
+  })
+  if(!response)
+    return res.json({ error: "User not found" })
 });
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
