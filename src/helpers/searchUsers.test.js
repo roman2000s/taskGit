0884c876df6b9d9ecf44799256ded4017558bf48
Eugene <skullet-on@mail.ru>
2019-10-14 10:12:10 +0300

    added async tests

--- src/helpers/searchUsers.test.js
+++ src/helpers/searchUsers.test.js
@@ -1,13 +1,38 @@
 import { searchUser, searchUsers } from './searchUsers';
 
-it('shoud search one user by login', () => {
-  expect.assertions(1);
-  return searchUser('roman')
-    .then(data => expect(data.login).toEqual('roman'));
-});
+describe('test searchUser function', () => {
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        json: () => {
+          return Promise.resolve({ login: 'test' })
+        }
+      })
+    })
+  })
+  
+  it('shoud search one user by login', async () => {
+    const { login } = await searchUser('test')
+    expect(window.fetch).toHaveBeenCalledWith('https://api.github.com/users/test')
+    expect(login).toBe('test')
+  });
+})
 
-it('shoud search some users by login', () => {
-  expect.assertions(1);
-  return searchUsers('roman')
-    .then(data => expect(data[0].login).toEqual('roman'));
-});
+describe('test searchUsers function', () => {
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        json: function () {
+          return Promise.resolve({ items: [{ login: 'test' }, { login: 'test2' }]})
+        }
+      })
+    })
+  })
+
+  it('shoud search many users by login', async () => {
+    const expected = [{ login: 'test' }, { login: 'test2' }]
+    const items = await searchUsers('test');
+    expect(window.fetch).toHaveBeenCalledWith('https://api.github.com/search/users?q=test')
+    expect(items).toEqual(expected)
+  });
+})
